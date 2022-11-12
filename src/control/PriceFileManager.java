d41b9fa41adc9a7cdb1726eace4d0e55319b5988
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-11-12 19:57:43 +0800

    STUDENT weekend prices added + database

--- src/control/PriceFileManager.java
+++ src/control/PriceFileManager.java
@@ -9,17 +9,41 @@ import java.util.*;
 public class PriceFileManager {
     public static void main(String[] args) {
         String path_student_weekday = "./../Database/Movie_Student_Weekday.csv";
+        String path_student_weekend = "./../Database/Movie_Student_Weekend.csv";
+
         String line = "";
 
-        // Age group first -> weekday / weekend -> which day on the weekday / weekend
+        // my Tries
         Trie student_weekday_prices = new Trie();
+        Trie student_weekend_prices = new Trie();
 
+        // weekday prices
         try (BufferedReader br = new BufferedReader(new FileReader(path_student_weekday))) {
             while ((line = br.readLine()) != null) {
                 String[] currentLine = line.split(",");
                 // insert student weekday prices in
                 if (currentLine.length != 0) {
                     student_weekday_prices.insertInformation(currentLine);
+
+                }
+
+                System.out.println(line);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // weekend prices
+        try (BufferedReader br = new BufferedReader(new FileReader(path_student_weekend))) {
+            while ((line = br.readLine()) != null) {
+                String[] currentLine = line.split(",");
+                // insert student weekday prices in
+                if (currentLine.length != 0) {
+                    student_weekend_prices.insertInformation(currentLine);
 
                 }
 

a3f96c42f602d662aa6ebf961c42c4e4e7fc3651
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-11-12 19:51:11 +0800

    Using Trie here instead

--- src/control/PriceFileManager.java
+++ src/control/PriceFileManager.java
@@ -1,30 +1,57 @@
 package control;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashMap;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class PriceFileManager {
+    public static void main(String[] args) {
+        String path_student_weekday = "./../Database/Movie_Student_Weekday.csv";
+        String line = "";
 
-    // our information could be split into :
-    // Movie type -> Cinema Class -> Ticket type -> date time
-    HashMap<String, HashMap<String, HashMap<String, String>>> PriceFiles = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+        // Age group first -> weekday / weekend -> which day on the weekday / weekend
+        Trie student_weekday_prices = new Trie();
 
-    public void loadData() throws FileNotFoundException {
-        File file = new File("./../Database/test.txt");
-        Scanner scan = new Scanner(file);
-        while (scan.hasNextLine()) {
-            String currentLine = scan.nextLine();
-            String[] PriceInformationArray = currentLine.split("-");
-            // populate the hashmap here
+        try (BufferedReader br = new BufferedReader(new FileReader(path_student_weekday))) {
+            while ((line = br.readLine()) != null) {
+                String[] currentLine = line.split(",");
+                // insert student weekday prices in
+                if (currentLine.length != 0) {
+                    student_weekday_prices.insertInformation(currentLine);
 
+                }
+
+                System.out.println(line);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
-    public ArrayList<Integer> updatePrice() {
+    public static class TrieNode {
+        Map<String, TrieNode> children = new HashMap<String, TrieNode>();
+    }
 
+    public static class Trie {
+        TrieNode root = new TrieNode();
+
+        public void insertInformation(String[] currentLine) {
+            TrieNode node = root;
+            for (String currentString : currentLine) {
+                if (!node.children.containsKey(currentString)) {
+                    TrieNode newNode = new TrieNode();
+                    node.children.put(currentString, newNode);
+                }
+
+            }
+        }
     }
 
 }

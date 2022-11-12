505085eba4bb265e3f9af8e991a41c815b75e60a
lawminghan <lawminghan2000@gmail.com>
2022-11-12 13:42:41 +0800

    Adam Initial Commit

---
+++ src/entity/Holiday.java
@@ -0,0 +1,24 @@
+package entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Holiday implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID =7L;
+	private LocalDate date;
+	
+	public Holiday(LocalDate d) {
+		date = d;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+}

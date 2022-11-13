7f565e21a59969a9500beeea1f243063bcc2198f
adam tan jun xiang <suwekforu@gmail.com>
2022-11-13 22:48:43 +0800

    javadocs

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"boundary"},{"l":"control"},{"l":"entity"}];updateSearchResults();

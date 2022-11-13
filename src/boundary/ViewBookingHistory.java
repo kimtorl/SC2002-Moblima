78a369680bf4eec2f006b14025e4e2053c2ca537
adam tan jun xiang <suwekforu@gmail.com>
2022-11-13 09:08:08 +0800

    adam

--- src/boundary/ViewBookingHistory.java
+++ src/boundary/ViewBookingHistory.java
@@ -1,8 +1,11 @@
 package boundary;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import control.TransactionManager;
+import entity.MovieGoer;
+import entity.Transaction;
 
 public class ViewBookingHistory implements Capability, Serializable {
 
@@ -17,6 +20,19 @@ public class ViewBookingHistory implements Capability, Serializable {
 	@Override
 	public void performCapability() {
 		// TODO Auto-generated method stub
+		ArrayList<Transaction> transList;
+		transList = transactionMgr.getAllTransaction();
+		
+		MovieGoer acc = (MovieGoer)MainApplication.currentAcc;
+		String name = acc.getName();
+		
+		for(int i = 0; i < transList.size(); i++)
+		{
+			if(name.equals(transList.get(i).getName()))
+			{
+				printTransaction(transList.get(i));
+			}
+		}
 
 	}
 
@@ -26,5 +42,36 @@ public class ViewBookingHistory implements Capability, Serializable {
 	public String toString() {
 		String str = "View Booking History";
 		return str;
+	}
+	
+	public void printTransaction(Transaction t) {
+		System.out.println("Transaction ID: "
+				+t.getTransactionID()
+				+"\nTransaction amount: "
+				+t.getTransactionAmount()
+				+"\nName: "
+				+t.getName()
+				+"\nMobile Number: "
+				+t.getMobileNumber()
+				+"\nEmail "
+				+t.getEmail()
+				+"\nDate & Time: "
+				+t.getDateTime()
+				+"\nMovie ID: "
+				+t.getMovieID()
+				+"\nCinema Code: "
+				+t.getCinemaCode()
+				+"\nSeat ID List: "
+				);
+		printSeatIDList(t);
+		System.out.println("______________________________");
+	}
+	
+	public void printSeatIDList(Transaction t) {
+		ArrayList<Integer> seatList = t.getSeatIDList();
+		for(int i = 0; i < seatList.size(); i++) {
+			System.out.print(seatList.get(i)+" | ");
+		}
+		return;
 	}
 }
